f58e0f19a41733e58eed848cfa46c9195b8c89b3
emily <nakanodepaula@gmail.com>
2022-09-27 09:28:22 -0300

    🎉 first commit

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import app from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = Number(process.env.PORT);
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));

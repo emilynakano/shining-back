1ae17519a94c80953afe94f31b546cc1bf8be244
emily <nakanodepaula@gmail.com>
2022-09-27 17:13:56 -0300

    feat: generate token

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -28,4 +28,7 @@ export async function loginUser ( dataUser: LoginUser ) {
 
     const passwordIsValid = bcrypt.compareSync(password, user.password);
     if(!passwordIsValid) throw errorUtils.unauthorizedError("credentials");
+
+    const token = jwt.sign({ id: user.id }, (process.env.JWT_SECRET_KEY as string));
+    return { token };
 }

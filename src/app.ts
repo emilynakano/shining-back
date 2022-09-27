80976ee2c67d6591c560695a1102f0ff98fe203a
emily <nakanodepaula@gmail.com>
2022-09-27 11:13:37 -0300

    feat: error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { json } from "express";
 import cors from "cors";
+import handleErrorMiddleware from "./middleware/handleErrorMiddleware";
 
 const app = express();
 
 app.use(json());
 app.use(cors());
+app.use(handleErrorMiddleware);
 
 export default app

b369e5d6d89817461d637da81fd4b2ea5b15b4d4
emily <nakanodepaula@gmail.com>
2022-09-27 14:30:00 -0300

    feat: encrypt password

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,14 +1,19 @@
 import { User } from "@prisma/client";
+
 import * as authRepository from "../repositories/authRepository";
 import * as errorUtils from "../utils/errorUtil";
+
+import bcrypt from "bcrypt";
 
 export type CreateUser =  Omit<User, "id">
 
 export async function registerUser ( dataUser: CreateUser ) {
-    const { email } = dataUser;
+    const { email, password } = dataUser;
 
     const user = await authRepository.findUserByEmail(email);
     if(user) throw errorUtils.conflictError("email");
 
-    await authRepository.registerUser(dataUser);
+    const hashPassword = bcrypt.hashSync(password, 10)
+
+    await authRepository.registerUser({...dataUser, password: hashPassword});
 }

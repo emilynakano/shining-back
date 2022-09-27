a860bbb4669653df8284e2f6de9e55f693141176
emily <nakanodepaula@gmail.com>
2022-09-27 18:46:04 -0300

    fix: change expiresIn

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -29,8 +29,8 @@ export async function loginUser ( dataUser: LoginUser ) {
     const passwordIsValid = bcrypt.compareSync(password, user.password);
     if(!passwordIsValid) throw errorUtils.unauthorizedError("credentials");
 
-    const token = jwt.sign({ id: user.id }, (process.env.ACCESS_TOKEN_SECRET as string), { expiresIn: '20s' });
-    const refreshToken = jwt.sign({ id: user.id }, (process.env.REFRESH_TOKEN_SECRET as string), { expiresIn: '20s' });
+    const token = jwt.sign({ id: user.id }, (process.env.ACCESS_TOKEN_SECRET as string), { expiresIn: '1h' });
+    const refreshToken = jwt.sign({ id: user.id }, (process.env.REFRESH_TOKEN_SECRET as string), { expiresIn: '1d' });
     
     return { token, refreshToken };
 }

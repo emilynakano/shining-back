9778c9fcf4a052b1128975bef7167e9cf78e21a3
emily <nakanodepaula@gmail.com>
2022-09-27 21:17:27 -0300

    feat: schemaMIddleware

---
+++ src/middleware/schemaMiddleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+
+export default async function schemaMiddleware (schema: any) {
+    return async (
+        req: Request, 
+        res: Response, 
+        next: NextFunction
+        ) => {
+        const { error } = schema.validate(req.body, { abortEarly: false });
+
+        if(error) {
+            const message: string[] = error.details.map((err: { message: string }) => err.message);
+            res.status(422).send(message)
+        }
+
+        next();
+    }
+}

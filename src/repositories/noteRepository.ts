dbb9b7ba7100ccce7426c01a7852bf9e8ea903ab
emily <nakanodepaula@gmail.com>
2022-10-03 23:28:28 -0300

    feat: conection between note service and note repository

---
+++ src/repositories/noteRepository.ts
@@ -0,0 +1,17 @@
+import prisma from '../config/database';
+
+export async function create(content: string) {
+
+}
+
+export async function getAll() {
+
+}
+
+export async function getToday() {
+
+}
+
+export async function review(id: number) {
+
+}
--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,15 +1,19 @@
+import * as noteRepository from '../repositories/noteRepository';
+
 export async function createNote(content: string) {
-  console.log(content);
+  await noteRepository.create(content);
 }
 
 export async function getNotes() {
-
+  const notes = await noteRepository.getAll();
+  return notes;
 }
 
 export async function getTodayNotes() {
-
+  const notes = await noteRepository.getToday();
+  return notes;
 }
 
 export async function reviewNote(id: number) {
-  console.log(id);
+  await noteRepository.review(id);
 }

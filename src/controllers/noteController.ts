457894b6e34c6db76433fb3bb4f28c1cfb789b8d
emily <nakanodepaula@gmail.com>
2022-10-03 23:22:33 -0300

    feat: conection between notes controller and notes service

--- src/controllers/noteController.ts
+++ src/controllers/noteController.ts
@@ -1,29 +1,43 @@
 import { Request, Response } from 'express';
 
+import * as noteService from '../services/noteService';
+
 export async function createNote(
   req: Request,
   res: Response,
 ) {
-  res.send('createnote');
+  const { content } = req.body;
+
+  await noteService.createNote(content);
+
+  res.status(201).send('Note registred successfully!');
 }
 
 export async function getNotes(
   req: Request,
   res: Response,
 ) {
-  res.send('getnotes');
+  const notes = await noteService.getNotes();
+
+  res.status(200).send(notes);
 }
 
 export async function getTodayNotes(
   req: Request,
   res: Response,
 ) {
-  res.send('gettodaynotes');
+  const notes = await noteService.getTodayNotes();
+
+  res.status(200).send(notes);
 }
 
 export async function reviewNote(
   req: Request,
   res: Response,
 ) {
-  res.send('revnotes');
+  const { id } = req.params;
+
+  await noteService.reviewNote(Number(id));
+
+  res.sendStatus(200);
 }
---
+++ src/services/noteService.ts
@@ -0,0 +1,15 @@
+export async function createNote(content: string) {
+  console.log(content);
+}
+
+export async function getNotes() {
+
+}
+
+export async function getTodayNotes() {
+
+}
+
+export async function reviewNote(id: number) {
+  console.log(id);
+}

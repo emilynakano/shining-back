c420e78bd31a17bd3ac9f95bd49aad521d7da6bc
emily <nakanodepaula@gmail.com>
2022-10-08 19:25:17 -0300

    fix: add ./ in dist

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import app from './app';
 
 dotenv.config();
 
-const PORT = Number(process.env.PORT);
+const PORT = Number(process.env.PORT) || 5000;
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));

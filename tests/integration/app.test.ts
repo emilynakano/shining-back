139d260de53ae5711ae9aa0b8ee834d421e3a71a
emily <nakanodepaula@gmail.com>
2022-10-09 23:41:50 -0300

    test: add after all to disconnect

--- tests/integration/app.test.ts
+++ tests/integration/app.test.ts
@@ -7,3 +7,15 @@ const agent = supertest(app);
 beforeEach(async () => {
   await prisma.$executeRaw`TRUNCATE TABLE users CASCADE;`;
 });
+
+afterAll(async () => {
+  await prisma.$disconnect();
+});
+
+describe('POST /sign-up', () => {
+  it('201: create user', async () => {
+    const result = await agent.post('/sign-in').send({ oi: 'oi' });
+
+    expect(result.status).toBe(201);
+  });
+});

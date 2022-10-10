822fed6ff3406821cba36381c85790356114dbcf
emily <nakanodepaula@gmail.com>
2022-10-10 01:38:40 -0300

    feat: middleware in note router when create a note

--- src/routes/notesRouter.ts
+++ src/routes/notesRouter.ts
@@ -2,11 +2,14 @@ import { Router } from 'express';
 import * as noteController from '../controllers/noteController';
 import tokenValidationMiddleware from '../middleware/tokenValidationMiddleware';
 
+import schemaMiddleware from '../middleware/schemaMiddleware';
+import { createNote } from '../schemas/noteSchema';
+
 const noteRouter = Router();
 
 noteRouter.use(tokenValidationMiddleware);
 
-noteRouter.post('/', noteController.createNote);
+noteRouter.post('/', schemaMiddleware(createNote), noteController.createNote);
 noteRouter.get('/', noteController.getNotes);
 noteRouter.get('/today', noteController.getTodayNotes);
 noteRouter.patch('/:id/review', noteController.reviewNote);

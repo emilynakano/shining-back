480d7c1e0f44ccdafee84989c290ba5c8dab2646
emily <nakanodepaula@gmail.com>
2022-11-05 22:23:35 -0300

    fix: remove update name from router

--- src/routes/notesRouter.ts
+++ src/routes/notesRouter.ts
@@ -10,7 +10,7 @@ const noteRouter = Router();
 noteRouter.use(tokenValidationMiddleware);
 
 noteRouter.post('/', schemaMiddleware(createNote), noteController.createNote);
-noteRouter.patch('/update/:id', schemaMiddleware(updateNote), noteController.editNote);
+noteRouter.patch('/:id', schemaMiddleware(updateNote), noteController.editNote);
 noteRouter.get('/', noteController.getNotes);
 noteRouter.get('/today', noteController.getTodayNotes);
 noteRouter.patch('/:id/review', noteController.reviewNote);

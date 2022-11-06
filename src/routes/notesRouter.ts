cc457b585def75f0c996742411bc0fcc48479170
emily <nakanodepaula@gmail.com>
2022-11-05 22:06:00 -0300

    feat: add joi validation in edit router

--- src/routes/notesRouter.ts
+++ src/routes/notesRouter.ts
@@ -3,14 +3,14 @@ import * as noteController from '../controllers/noteController';
 import tokenValidationMiddleware from '../middleware/tokenValidationMiddleware';
 
 import schemaMiddleware from '../middleware/schemaMiddleware';
-import { createNote } from '../schemas/noteSchema';
+import { createNote, updateNote } from '../schemas/noteSchema';
 
 const noteRouter = Router();
 
 noteRouter.use(tokenValidationMiddleware);
 
 noteRouter.post('/', schemaMiddleware(createNote), noteController.createNote);
-noteRouter.patch('/update/:id', noteController.editNote);
+noteRouter.patch('/update/:id', schemaMiddleware(updateNote), noteController.editNote);
 noteRouter.get('/', noteController.getNotes);
 noteRouter.get('/today', noteController.getTodayNotes);
 noteRouter.patch('/:id/review', noteController.reviewNote);
